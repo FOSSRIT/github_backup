ac86ea4979737bfee3fe15a9f77c80fd4b39cb5d
Ryan Brown <ryansb@csh.rit.edu>
2013-04-10 14:41:36 -0400

    Accidentally prepended a $

--- fossrit_backup.sh
+++ fossrit_backup.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd $1
-$GITBACKPATH=/usr/local/bin/github_backup
+GITBACKPATH=/usr/local/bin/github_backup
 
 USERS=`python -c "import requests
 for u in requests.get('https://api.github.com/orgs/FOSSRIT/public_members').json():

c5a827f4d4ce0008bd5f4e42f06ee84c443d2ab0
Ryan Brown <ryansb@csh.rit.edu>
2013-04-16 13:59:09 -0400

    Add gist backup script

--- fossrit_gist_backup.sh
+++ fossrit_gist_backup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-cd /home/gitback/backups
-
 USERS=`python -c "import requests
 for u in requests.get('https://api.github.com/orgs/FOSSRIT/public_members').json():
     print u['login']"`
 
+cd /home/gitback/gist_backups
 for user in $USERS
 do
-    /home/gitback/github_backup.sh $user
+    /home/gitback/github_backup/gist_backup.sh $user
 done
-
-/home/gitback/github_backup.sh FOSSRIT org
